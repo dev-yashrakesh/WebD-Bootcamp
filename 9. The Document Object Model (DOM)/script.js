e7f0313d7ac43b6421e7abb2cc529868966f5d27
Yash Rakesh <82806001+dev-yashrakesh@users.noreply.github.com>
2023-05-24 07:34:16 +0530

    Add files via upload

---
+++ 9. The Document Object Model (DOM)/script.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("li")[2].style.color="purple";
+document.getElementsByTagName("li").length;
+document.querySelector("button").style.backgroundColor="purple";
+document.querySelector("button").classList.add;
